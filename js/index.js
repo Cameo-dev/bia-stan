3c4b4e0ffd4e6cc5e68c13cfc16ef0b46c576ca0
JbIPS <JbIPS@users.noreply.github.com>
2022-01-20 23:08:02 +0000

    deploy: dd2a7c1c715a4bd373ff0e77af85b909d3003767

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
+window.addEventListener('hashchange', function() {
+	let hash = window.location.hash;
+	if(hash) goToStep(hash)
+}, false);
+
 function initPage() {
 	if(window.location.pathname === '/') {
+		let hash = window.location.hash;
+		if(hash) goToStep(hash);
+
+		const dropzones = Array.from(document.getElementsByClassName('dropzone'));
+		dropzones.forEach(zone => {
+			zone.ondrop = dropHandler;
+			zone.ondragover = dragOverHandler;
+			zone.ondragenter = dragEnterHandler;
+			zone.ondragleave = dragLeaveHandler;
+		})
 		
+		Array.from(document.getElementsByClassName('file-select')).forEach(fs => {
+			fs.addEventListener( 'change', function( e ) {
+				const file = e.target.files[0];
+				addUploadedFile(file);
+			});
+		});
 	} else if(window.location.pathname === '/bia') {
 		const tables = document.querySelectorAll('th');
 		for(let i = 0; i < tables.length; i++) {
@@ -62,5 +83,55 @@ if(document.readyState === 'loading') {
 // Buttons
 function goToStep(step) {
 	document.querySelector('.step:not([hidden])').setAttribute('hidden', '');
-	document.getElementById(`step-${step}`).removeAttribute('hidden');
+	document.getElementById(`step-${step.replace("#", "")}`).removeAttribute('hidden');
+}
+
+// File handling
+function addUploadedFile(file) {
+	const dropzone = document.querySelector('.step:not(hidden) .dropzone');
+	dropzone.textContent = file.name;
+}
+
+// Drop
+function dropHandler(event) {
+	console.log("DROP", event);
+	event.preventDefault();
+	event.stopPropagation();
+	if (event.dataTransfer.items) {
+		// Use DataTransferItemList interface to access the file(s)
+		for (var i = 0; i < event.dataTransfer.items.length; i++) {
+			// If dropped items aren't files, reject them
+			if (event.dataTransfer.items[i].kind === 'file') {
+				var file = event.dataTransfer.items[i].getAsFile();
+				addUploadedFile(file);
+			}
+		}
+	} else {
+		// Use DataTransfer interface to access the file(s)
+		for (var i = 0; i < event.dataTransfer.files.length; i++) {
+			console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
+		}
+	}
+	event.target.classList.remove('dragover');
+}
+
+function dragOverHandler(event) {
+	//console.log("DRAG OVER", event)
+	event.preventDefault();
+	event.stopPropagation();
+	//event.target.classList.add('dragover');
+}
+
+function dragEnterHandler(event) {
+	console.log("DRAG ENTER", event)
+	event.preventDefault();
+	event.stopPropagation();
+	event.target.classList.add('dragover');
+}
+
+function dragLeaveHandler(event) {
+	console.log("DRAG LEAVE", event);
+	event.preventDefault();
+	event.stopPropagation();
+	event.target.classList.remove('dragover');
 }
