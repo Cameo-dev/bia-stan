c6c9d18436419da5767350db3c4e30cbcc4abdc7
JbIPS <JbIPS@users.noreply.github.com>
2022-01-20 13:24:32 +0000

    deploy: 83f001e3c734fc4a56eed028cd283a9dc046d798

--- js/index.js
+++ js/index.js
@@ -1,42 +1,34 @@
 function initPage() {
-	//const inputs = document.querySelectorAll('input');
-	//for(let i = 0; i < inputs.length; i++) {
-		//const input = inputs.item(i);
-		//input.addEventListener('change', (e) => {
-			//const inp = e.target;
-			//inp.classList.toggle('erreur', inp.validity.patternMismatch);
-			//inp.classList.toggle('valide', !inp.validity.patternMismatch);
-		//});
-	//}
-	
-	const tables = document.querySelectorAll('th');
-	for(let i = 0; i < tables.length; i++) {
-		const input = tables.item(i);
-		input.addEventListener('click', ({target}) => {
-			// Get index
-			const columnIndex = [...target.parentElement.children].indexOf(target);
-			const table = target.parentElement.parentElement.parentElement;
-			// Clean up
-			table.querySelectorAll('.table-active').forEach(active => active.classList.remove('table-active'));
-
-			table.querySelectorAll(`td:nth-child(${columnIndex + 1})`)
-			.forEach(td => td.classList.add('table-active'));
-			target.classList.add('table-active');
-		})
-	}
-
-	function deductFromSS(ssNumber, civility, birthday) {
-		ssNumber.addEventListener('change', () => {
-			if(!ssNumber.validity.patternMismatch) {
-				Array.from(civility.children)
-					.filter(c => c.type == 'radio')[parseInt(ssNumber.value.charAt(0), 10) - 1].checked = true;
-				const birthdate = new Date(Date.UTC(1900 + parseInt(ssNumber.value.substr(1,2), 10), parseInt(ssNumber.value.substr(3, 2), 10) - 1)); // Get month and year from SS
-				birthday.value = birthdate.toISOString().split('T')[0];
-			}
-		})
-	}
-
 	if(window.location.pathname === '/') {
+		
+	} else if(window.location.pathname === '/bia') {
+		const tables = document.querySelectorAll('th');
+		for(let i = 0; i < tables.length; i++) {
+			const input = tables.item(i);
+			input.addEventListener('click', ({target}) => {
+				// Get index
+				const columnIndex = [...target.parentElement.children].indexOf(target);
+				const table = target.parentElement.parentElement.parentElement;
+				// Clean up
+				table.querySelectorAll('.table-active').forEach(active => active.classList.remove('table-active'));
+
+				table.querySelectorAll(`td:nth-child(${columnIndex + 1})`)
+					.forEach(td => td.classList.add('table-active'));
+				target.classList.add('table-active');
+			})
+		}
+
+		function deductFromSS(ssNumber, civility, birthday) {
+			ssNumber.addEventListener('change', () => {
+				if(!ssNumber.validity.patternMismatch) {
+					Array.from(civility.children)
+						.filter(c => c.type == 'radio')[parseInt(ssNumber.value.charAt(0), 10) - 1].checked = true;
+					const birthdate = new Date(Date.UTC(1900 + parseInt(ssNumber.value.substr(1,2), 10), parseInt(ssNumber.value.substr(3, 2), 10) - 1)); // Get month and year from SS
+					birthday.value = birthdate.toISOString().split('T')[0];
+				}
+			})
+		}
+
 		deductFromSS(
 			document.getElementById('socialsecurity'),
 			document.getElementById('civility'),
@@ -48,21 +40,27 @@ function initPage() {
 			document.getElementById('birthday-partner')
 		);
 		//deductFromSS(
-			//document.getElementById('socialsecurity-child'),
-			//document.getElementById('civility-child'),
-			//document.getElementById('birthday-child')
+		//document.getElementById('socialsecurity-child'),
+		//document.getElementById('civility-child'),
+		//document.getElementById('birthday-child')
 		//);
-	}
 
-	const teletransmission = document.getElementById('teletransmission');
-	const teletransmissionSet = document.getElementById('teletransmission-fieldset');
-	teletransmission.addEventListener('change', () => {
-		teletransmissionSet.classList.toggle('d-none', !teletransmission.checked);
-	});
+		const teletransmission = document.getElementById('teletransmission');
+		const teletransmissionSet = document.getElementById('teletransmission-fieldset');
+		teletransmission.addEventListener('change', () => {
+			teletransmissionSet.classList.toggle('d-none', !teletransmission.checked);
+		});
+	}
 }
 
 if(document.readyState === 'loading') {
   window.addEventListener('DOMContentLoaded', initPage);
 } else {
   initPage();
+}
+
+// Buttons
+function goToStep(step) {
+	document.querySelector('.step:not([hidden])').setAttribute('hidden', '');
+	document.getElementById(`step-${step}`).removeAttribute('hidden');
 }
